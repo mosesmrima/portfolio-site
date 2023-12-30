5269d1d40c8b425042ac76300a678f9e369aa0b2
mrima <mrimamss@gmail.com>
2023-12-30 15:36:50 +0300

    default font

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -13,7 +13,7 @@ export default async function Article({params}) {
 
   return (
     <div className={"w-screen flex justify-center"}>
-      <Card className={"prose prose-lg sm:prose-xl max-w-none prose-neutral dark:prose-invert  w-11/12 sm:w-10/12 flex flex-col items-center  py-8 px-4"}>
+      <Card className={"prose max-w-none prose-neutral dark:prose-invert  w-11/12 sm:w-10/12 flex flex-col items-center  py-8 px-4"}>
         <h3 className={`text-center ${press_start_2p.className}`}>{post.title}</h3>
         <PortableText value={post.body} />
       </Card>
