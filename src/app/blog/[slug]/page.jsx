087945bcd59970d608bc23bc460ca465004d7ab0
mrima <mrimamss@gmail.com>
2023-12-30 15:09:03 +0300

    fix font

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,7 +1,6 @@
 import {clientConfig} from '@/utils/sanityUtils'
 import {PortableText} from '@portabletext/react'
 import {Card} from '@nextui-org/react'
-import {press_start_2p} from '@/app/fonts'
 export const revalidate = false
 
 export default async function Article({params}) {
@@ -14,7 +13,7 @@ export default async function Article({params}) {
   return (
     <div className={"w-screen flex justify-center"}>
       <Card className={"prose prose-lg md:prose-lg lg:prose-xl max-w-none prose-neutral dark:prose-invert  w-11/12 sm:w-10/12 flex flex-col items-center  py-8 px-4"}>
-        <h1 className={`${press_start_2p.className} text-center`}>{post.title}</h1>
+        <h1 className={"text-md text-center"}>{post.title}</h1>
         <PortableText value={post.body} />
       </Card>
     </div>
