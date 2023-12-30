d0325b87c23c427c89b2f1b624fffea1b01e7263
mrima <mrimamss@gmail.com>
2023-12-30 14:48:03 +0300

    minor style edits

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -13,7 +13,7 @@ export default async function Article({params}) {
 
   return (
     <div className={"w-screen flex justify-center"}>
-      <Card className={"w-11/12 sm:w-10/12 flex flex-col items-center gap-2 py-8 px-4"}>
+      <Card className={"prose prose-lg md:prose-lg lg:prose-xl max-w-none prose-neutral dark:prose-invert  w-11/12 sm:w-10/12 flex flex-col items-center  py-8 px-4"}>
         <h1 className={`${press_start_2p.className} text-center`}>{post.title}</h1>
         <PortableText value={post.body} />
       </Card>
