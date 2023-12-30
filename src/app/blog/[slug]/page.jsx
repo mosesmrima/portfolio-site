c7fc6cbc835d38c5125f37d6d47373e574c8b7f9
mrima <mrimamss@gmail.com>
2023-12-30 15:16:36 +0300

    revalidate fetch

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,7 +1,7 @@
 import {clientConfig} from '@/utils/sanityUtils'
 import {PortableText} from '@portabletext/react'
 import {Card} from '@nextui-org/react'
-export const revalidate = false
+export const revalidate = 10;
 
 export default async function Article({params}) {
   const query = `*[_type == "post" && slug.current == $slug][0]`;
@@ -13,7 +13,7 @@ export default async function Article({params}) {
   return (
     <div className={"w-screen flex justify-center"}>
       <Card className={"prose prose-lg sm:prose-xl max-w-none prose-neutral dark:prose-invert  w-11/12 sm:w-10/12 flex flex-col items-center  py-8 px-4"}>
-        <h1 className={"text-md text-center"}>{post.title}</h1>
+        <h1 className={"text-center"}>{post.title}</h1>
         <PortableText value={post.body} />
       </Card>
     </div>
