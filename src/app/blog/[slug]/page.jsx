19f0b389c13f54ab5e3c4128d312a5403049a20e
mrima <mrimamss@gmail.com>
2023-12-30 15:11:34 +0300

    fix font

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -12,7 +12,7 @@ export default async function Article({params}) {
 
   return (
     <div className={"w-screen flex justify-center"}>
-      <Card className={"prose prose-lg md:prose-lg lg:prose-xl max-w-none prose-neutral dark:prose-invert  w-11/12 sm:w-10/12 flex flex-col items-center  py-8 px-4"}>
+      <Card className={"prose prose-lg sm:prose-xl max-w-none prose-neutral dark:prose-invert  w-11/12 sm:w-10/12 flex flex-col items-center  py-8 px-4"}>
         <h1 className={"text-md text-center"}>{post.title}</h1>
         <PortableText value={post.body} />
       </Card>
