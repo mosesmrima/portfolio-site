7895cd51687e4c267a738325e3d8e2562a4fba39
mrima <mrimamss@gmail.com>
2024-01-19 13:46:27 +0300

    change max dimensions

--- src/app/_components/postComponents/ImagePortableCom.jsx
+++ src/app/_components/postComponents/ImagePortableCom.jsx
@@ -4,14 +4,17 @@ import {urlFor} from '@/utils/sanityUtils';
 
 const ImagePortableCom = ({ value }) => {
   if (!value?.asset?._ref) {
-    return null;
+    return null; // Return null instead of value if there's no image reference
   }
 
   // Define a maximum size for the images
-  const maxWidth = 800;
-  const maxHeight = 600;
+  const maxWidth = 500; // max width in pixels
+  const maxHeight = 375; // max height in pixels
+
+  // Calculate aspect ratio of the image
   const aspectRatio = value?.imageWidth / value?.imageHeight;
 
+  // Calculate responsive width and height
   let width = maxWidth;
   let height = width / aspectRatio;
 
@@ -21,20 +24,19 @@ const ImagePortableCom = ({ value }) => {
   }
 
   return (
-    <div className='flex flex-col justify-center items-center'>
+    <div className='flex justify-center flex-col'>
       <Image
         alt={value.alt || "Image"}
         loading="lazy"
         src={`${urlFor(value)}`}
-        height={height}
-        width={width}
+        height={height} // Use calculated height
+        width={width} // Use calculated width
         objectFit='contain'
-        layout="responsive"
+        layout="responsive" // Make the image responsive
       />
-      {value.alt && <i className='text-gray-600 text-center mt-2'>{value.alt}</i>}
+      {value.alt && <i className='text-gray-600 flex justify-center text-center'>{value.alt}</i>}
     </div>
   );
-
 }
 
 export default ImagePortableCom;
