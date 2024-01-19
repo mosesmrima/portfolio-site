c47b1773ef5fcf34a71b6eadf3e9f65280c34e98
mrima <mrimamss@gmail.com>
2024-01-19 12:47:24 +0300

    fix image render

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -2,6 +2,7 @@ import {clientConfig} from '@/utils/sanityUtils'
 import {PortableText} from '@portabletext/react'
 import {Card} from '@nextui-org/react'
 import {press_start_2p} from '@/app/fonts'
+import ArticleContent from '@/app/_components/postComponents/ArticleContent'
 export const revalidate = 10;
 
 export default async function Article({params}) {
@@ -15,7 +16,7 @@ export default async function Article({params}) {
     <div className={"w-screen flex justify-center"}>
       <Card className={"prose max-w-none prose-neutral dark:prose-invert  w-11/12 sm:w-10/12 flex flex-col items-center  py-8 px-4"}>
         <h3 className={`text-center ${press_start_2p.className}`}>{post.title}</h3>
-        <PortableText value={post.body} />
+        <ArticleContent postContent={post.body}/>
       </Card>
     </div>
   );
